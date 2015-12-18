07e4e81df233f8073f4848f89f4aea8c9ac51076
hamuhamu <hrdng4922000@yahoo.co.jp>
2015-12-19 01:03:27 +0900

    zero.goの追加

---
+++ zero.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b bool
+	var i int
+	var s string
+
+	fmt.Println("bool型のゼロ値:", b)
+	fmt.Println("int型のゼロ値:", i)
+	fmt.Println("string型のゼロ値:", s)
+}
